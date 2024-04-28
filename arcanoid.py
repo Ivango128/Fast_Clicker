781a144ef5a66e9109b90f18792aa00da93d5f3f
Ivango128 <poplayhin2002@gmail.com>
2024-04-28 14:49:01 +0500

    Арканойд часть 1

---
+++ arcanoid.py
@@ -0,0 +1,95 @@
+import pygame
+
+pygame.init()
+
+back = (255, 229, 45)
+window_w = 500
+window_h = 500
+main_window = pygame.display.set_mode((window_w, window_h))
+main_window.fill(back)
+
+clock = pygame.time.Clock()
+
+
+class RectCard():
+    def __init__(self, x=0, y=0, width=10, height=10, color=back, frame_color=(0, 0, 0), thickness=2):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.rect.x = x
+        self.rect.y = y
+        self.fill_color = color
+        self.frame_color = frame_color
+        self.thickness = thickness
+
+    def fill(self):
+        pygame.draw.rect(main_window, self.fill_color, self.rect)
+
+    def fill_new_color(self, new_color):
+        pygame.draw.rect(main_window, new_color, self.rect)
+
+    def outline(self):
+        pygame.draw.rect(main_window, self.frame_color, self.rect, self.thickness)
+
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)
+
+
+class LableCard(RectCard):
+    def set_text(self, text):
+        self.lable = pygame.font.SysFont('Arial', 28).render(text, True, (0, 0, 0))
+
+    def draw_text(self, shift_x, shift_y):
+        self.fill()
+        self.outline()
+        main_window.blit(self.lable, (self.rect.x + shift_x, self.rect.y + shift_y))
+
+    def draw(self):
+        self.fill()
+        self.outline()
+
+
+def SpriteImg(RectCard):
+    def __init__(self, x, y, width, height, puth_img, color=back, frame_color=(0, 0, 0), thickness=0):
+        super().__init__(self, x, y, width, height, color, frame_color, thickness)
+        self.image = pygame.image.load(puth_img)
+
+    def draw_img(self):
+        main_window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+# размер монстра 50х50
+# 50*9 = 450 9 монстров
+# 500 - 450 = 50 общее растояние между монстрами
+# 50 // (9-1) = 6 остаток 2
+# от левого отсупить 1
+
+width_monstr = 50
+height_monstr = 50
+count_monsters = 9
+
+x = (window_w - (count_monsters * width_monstr)) % (count_monsters - 1) // 2
+y = 1
+
+row = 3
+
+monsters = []
+
+for i in range(row):
+    for j in range(count_monsters):
+        monster = SpriteImg(x=x, y=y, width=width_monstr, height=height_monstr, puth_img='enemy.png')
+        monsters.append(monster)
+        x += width_monstr + ((window_w - (count_monsters * width_monstr)) // (count_monsters - 1))
+    x = (window_w - (count_monsters * width_monstr)) % (count_monsters - 1) // 2 + width_monstr // 2
+    y += height_monstr + 6
+    count_monsters -= 1
+
+run = True
+while run:
+    for monster in monsters:
+        monster.draw_img()
+
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            run = False
+
+    clock.tick(60)
+    pygame.display.update()
